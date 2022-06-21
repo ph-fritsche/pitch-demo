7e8192a71b4a0470d71f8c9a5336f2845e527ff8
Philipp Fritsche <ph.fritsche@gmail.com>
2022-06-21 11:22:23 +0000

    create Todo entity

---
+++ src/Entity/Todo.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TodoRepository::class)]
+class Todo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 4096, nullable: true)]
+    private $description;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $due;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDue(): ?\DateTimeInterface
+    {
+        return $this->due;
+    }
+
+    public function setDue(?\DateTimeInterface $due): self
+    {
+        $this->due = $due;
+
+        return $this;
+    }
+}
